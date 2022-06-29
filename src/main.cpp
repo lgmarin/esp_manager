727487f7a9ed182f5c6eb093d108d83adeaa6db2
lgmarin <11378945+lgmarin@users.noreply.github.com>
2022-06-29 11:28:26 -0300

    Configure captive portal

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,36 @@
 #include <Arduino.h>
+#include <DNSServer.h>
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <LittleFS.h>
+
+AsyncWebServer server(80);
+DNSServer dnsServer;
+
+const char* device = "esp-manager";
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+// Request Handlers
+void notFound(AsyncWebServerRequest *request) {
+    request->send(404, "text/plain", "Not found");
+}
+
+void openCaptivePortal(){
+  WiFi.softAP(device);
+  dnsServer.start(53, "*", WiFi.softAPIP());
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  //
+  server.serveStatic("/", LittleFS, "/").setDefaultFile("index.htm");
+
+  server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  dnsServer.processNextRequest();
 }
