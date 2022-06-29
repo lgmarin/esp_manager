5c35ba5da2268a6763854341c2ca10d9137e09c8
lgmarin <11378945+lgmarin@users.noreply.github.com>
2022-06-29 14:41:04 -0300

    Configure Captive Portal Function and Routes

--- src/main.cpp
+++ src/main.cpp
@@ -17,17 +17,32 @@ void notFound(AsyncWebServerRequest *request) {
     request->send(404, "text/plain", "Not found");
 }
 
-void openCaptivePortal(){
-  WiFi.softAP(device);
-  dnsServer.start(53, "*", WiFi.softAPIP());
+bool openCaptivePortal(){
+  Serial.print("Starting soft-AP ... ");
+  if(WiFi.softAP(device)){
+    Serial.println("SofAP Started!");
+    Serial.print("Soft-AP IP address = ");
+    Serial.println(WiFi.softAPIP());
+    dnsServer.start(53, "*", WiFi.softAPIP());
+    return true;
+  }
+  return false;
 }
 
 void setup() {
   Serial.begin(115200);
-
-  //
+  // Setup LittleFS
   server.serveStatic("/", LittleFS, "/").setDefaultFile("index.htm");
 
+  // Open Captive portal and configure Wifi Credentials
+  if (openCaptivePortal())
+  {
+    //Serve configuration page
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+      request->send(LittleFS, "/wifi.html", "text/html", false);
+  }).setFilter(ON_AP_FILTER);
+  }
+  
   server.begin();
 }
 
