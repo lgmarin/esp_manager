8e9788d27a38be35ef96263273141f05f1767643
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-09-20 15:46:20 -0300

    Wifi and Device Config

--- include/ConfigManager.h
+++ include/ConfigManager.h
@@ -2,7 +2,39 @@
 #define ConfigManager_h
 
 #include <LittleFS.h>
+#include <ESP8266WiFi.h>
 #include "definitions.h"
+
+// CONFIGURATIONS STRUCTS
+typedef struct
+{
+  char wifi_ssid[32];
+  char wifi_pw  [64];
+} WiFi_Credential;
+
+typedef struct
+{
+  IPAddress ip_addr{};
+  IPAddress gw_addr{};
+  IPAddress mask{};
+} IP_Config;
+
+typedef struct
+{
+  WiFi_Credential   WiFi_cred{};
+  IP_Config         IP_config{};
+  bool              dyn_ip = false;
+  uint16_t          checksum{};
+} Wifi_Config;
+
+typedef struct
+{
+  char       host_name[24]{};
+  bool       ap_mode = false;
+  uint16_t   air_value{};
+  uint16_t   wat_value{};
+  uint16_t   checksum{};
+} Device_Config;
 
 /**
  * @brief ConfigManager - Creates a configuration structure
@@ -11,7 +43,7 @@
  */
 class ConfigManager
 {
-protected:
+private:
     bool _debug = false;
     void _listFSFiles(String dir_path);
     bool _initFS(bool listFiles);
@@ -19,14 +51,21 @@ protected:
     bool _loadFSData(void *str_Config, size_t size, const char* filename);
     bool _saveFSData(void *str_Config, size_t size, const char* filename);
     uint16_t _calcChecksum(uint8_t* address, uint16_t sizeToCalc);
-};
+    bool _initDeviceConfiguration();
 
-class WifiConfig : ConfigManager
-{
 public:
-    bool isDebug();
-    WifiConfig();
-    WifiConfig(bool debug);
+    //Constructor
+    ConfigManager();
+    ConfigManager(bool debug);
+
+    //Public Methods
+    bool loadWifiConfig();
+    bool storeWifiConfig(String SSID, String password, bool dyn_ip, IPAddress ip, IPAddress gw, IPAddress mask);
+    bool removeWifiConfig();
+
+    bool loadDeviceConfig();
+    bool storeDeviceConfig(String host_name, String air_v, String wat_v, bool apmode);
+    bool removeDeviceConfig();
 };
 
 #endif
--- src/ConfigManager.cpp
+++ src/ConfigManager.cpp
@@ -1,5 +1,26 @@
 #include "ConfigManager.h"
 
+Wifi_Config        Wifi_config;
+Device_Config      Device_config;
+
+const char* wifi_config_file PROGMEM = "/wifi.cfg";
+const char* device_config_file PROGMEM = "/device.cfg";
+
+/* ConfigManager CONSTRUCTOR */
+
+ConfigManager::ConfigManager()
+{
+  _debug = false;
+  _initFS(false);
+}
+
+ConfigManager::ConfigManager(bool debug)
+{
+  _debug = debug;
+  _initFS(debug);
+}
+
+
 /*!
  *  @brief  Return the content of dir in the FS on the Serial Console.
  *  @param  string Directory path.
@@ -129,23 +150,192 @@ uint16_t ConfigManager::_calcChecksum(uint8_t* address, uint16_t sizeToCalc)
   return checkSum;
 }
 
-
-
-WifiConfig::WifiConfig()
+/* ConfigManager PUBLIC METHODS */
+/*!
+ *  @brief  Load WifiConfiguration saved in LitteFS.
+ *  @return Returns true if configuration loaded successfully.
+ */
+bool ConfigManager::loadWifiConfig()
 {
-  _debug = false;
-  _initFS(false);
+  if(_loadFSData(&Wifi_config, sizeof(Wifi_config), (char*) wifi_config_file))
+  {
+    if ( Wifi_config.checksum != _calcChecksum( (uint8_t*) &Wifi_config, sizeof(Wifi_config) - sizeof(Wifi_config.checksum) ) )
+    {
+      Serial.print(F("\n[ERROR]: Wifi Credentials checksum wrong!"));
+      return false;
+    }
+
+    // Don't permit NULL SSID and password len < MIN_AP_PASSWORD_SIZE (8)
+    if ((String(Wifi_config.WiFi_cred.wifi_ssid) == "") || (String(Wifi_config.WiFi_cred.wifi_pw) == ""))
+    {
+      Serial.print(F("\n[ERROR]: Wifi SSID or Password is empty!"));
+      return false;
+    }
+    Serial.print(F("\n[INFO]: Wifi Config File Read. Checksum ok."));
+    return true; 
+  }
+  else
+  {
+    Serial.print(F("\n[ERROR]: Could not read Wifi Config File."));
+    return false;
+  }
 }
 
-WifiConfig::WifiConfig(bool debug)
+/*!
+ *  @brief  Store WifiConfiguration into LitteFS.
+ *  @return Returns true if configuration saved successfully.
+ */
+bool ConfigManager::storeWifiConfig(String SSID, String password, bool dyn_ip, IPAddress ip, IPAddress gw, IPAddress mask)
 {
-  _debug = debug;
-  _initFS(debug);
+  //memset(&Wifi_config, 0, sizeof(Wifi_config));
+
+  //SAVE SSID
+  if (strlen(SSID.c_str()) < sizeof(Wifi_config.WiFi_cred.wifi_ssid) - 1)
+    strcpy(Wifi_config.WiFi_cred.wifi_ssid, SSID.c_str());
+  else
+    strncpy(Wifi_config.WiFi_cred.wifi_ssid, SSID.c_str(), sizeof(Wifi_config.WiFi_cred.wifi_ssid) - 1);
+
+  //SAVE PWD
+  if (strlen(password.c_str()) < sizeof(Wifi_config.WiFi_cred.wifi_pw) - 1)
+    strcpy(Wifi_config.WiFi_cred.wifi_pw, password.c_str());
+  else
+    strncpy(Wifi_config.WiFi_cred.wifi_pw, password.c_str(), sizeof(Wifi_config.WiFi_cred.wifi_pw) - 1);  
+
+  if ((String(Wifi_config.WiFi_cred.wifi_ssid) == "") && (String(Wifi_config.WiFi_cred.wifi_pw) == ""))
+    Serial.println(F("[WARNING]: Null SSID or Password!"));
+
+  //SAVE IP
+  Wifi_config.IP_config.ip_addr = ip;
+  Wifi_config.IP_config.gw_addr = gw;
+  Wifi_config.IP_config.mask = mask;
+
+  Wifi_config.dyn_ip = dyn_ip;
+  
+  //Calculate checksum and save credentials
+  Wifi_config.checksum = _calcChecksum((uint8_t*) &Wifi_config, sizeof(Wifi_config) - sizeof(Wifi_config.checksum));
+  if (_saveFSData(&Wifi_config, sizeof(Wifi_config), (char*) wifi_config_file))
+  {
+    Serial.print(F("\n[INFO]: Wifi Credentials file saved!"));
+    return true;
+  }
+
+  Serial.print(F("\n[ERROR]: Could not store Wifi Config File"));
+  return false;
+}
+
+/*!
+ *  @brief  Remove WifiConfiguration from LitteFS.
+ *  @return Returns true if configuration removed successfully.
+ */
+bool ConfigManager::removeWifiConfig()
+{
+  if(_removeFSData(wifi_config_file))
+    return true;
+
+  return false;
+}
+
+///
+///     DEVICE CONFIGURATION FILE
+///
+
+/*!
+ *  @brief  Load Device Configuration saved in LitteFS.
+ *  @return Returns true if configuration loaded successfully.
+ */
+bool ConfigManager::loadDeviceConfig()
+{
+  if(_loadFSData(&Device_config, sizeof(Device_config), (char*) device_config_file))
+  {
+    if ( Device_config.checksum != _calcChecksum( (uint8_t*) &Device_config, sizeof(Device_config) - sizeof(Device_config.checksum) ) )
+    {
+      Serial.print(F("\n[ERROR]: Device config checksum wrong!"));
+      return false;
+    }
+
+    if ((String(Device_config.host_name) == ""))
+    {
+      Serial.print(F("\n[ERROR]: Hostname is empty, using default!"));
+      if (strlen(String(DEFAULT_HOSTNAME).c_str()) < sizeof(Device_config.host_name) - 1)
+        strcpy(Device_config.host_name, String(DEFAULT_HOSTNAME).c_str());
+      else
+        strncpy(Device_config.host_name, String(DEFAULT_HOSTNAME).c_str(), sizeof(Device_config.host_name) - 1);
+    }
+    Serial.print(F("\n[INFO]: Device Config File Read. Checksum ok."));
+    return true; 
+  }
+  else
+  {
+    Serial.print(F("\n[ERROR]: Could not read device Config File."));
+    return false;
+  }
+}
+
+/*!
+ *  @brief  Store Device Configuration into LitteFS.
+ *  @return Returns true if configuration saved successfully.
+ */
+bool ConfigManager::storeDeviceConfig(String host_name, String air_v, String wat_v, bool apmode)
+{
+  //memset(&Device_config, 0, sizeof(Device_config));
+
+  //SAVE HOSTNAME
+  if (strlen(host_name.c_str()) < sizeof(Device_config.host_name) - 1)
+    strcpy(Device_config.host_name, host_name.c_str());
+  else
+    strncpy(Device_config.host_name, host_name.c_str(), sizeof(Device_config.host_name) - 1);
+
+  if ((String(Device_config.host_name) == ""))
+    Serial.println(F("[WARNING]: Null hostname!"));
+
+  //SAVE AIR_V
+  Device_config.air_value = air_v.toInt();
+
+  //SAVE WAT_V
+  Device_config.wat_value = wat_v.toInt();
+
+  //SAVE AP_MODE
+  Device_config.ap_mode = apmode;
+
+  //Calculate checksum and save credentials
+  Device_config.checksum = _calcChecksum((uint8_t*) &Device_config, sizeof(Device_config) - sizeof(Device_config.checksum));
+  if (_saveFSData(&Device_config, sizeof(Device_config), (char*) device_config_file))
+  {
+    Serial.print(F("\n[INFO]: Device config file saved!"));
+    return true;
+  }
+
+  Serial.print(F("\n[ERROR]: Could not store Devices Config File"));
+  return false;
 }
 
 
-bool WifiConfig::isDebug()
+/*!
+ *  @brief  Remove Device Configuration from LitteFS.
+ *  @return Returns true if configuration removed successfully.
+ */
+bool ConfigManager::removeDeviceConfig()
 {
-  Serial.print(_debug);
-  return _debug;
+  if(_removeFSData((char*) device_config_file))
+    return true;
+
+  return false;
+}
+
+
+/*!
+ *  @brief  Set parameters to factory default (see definitions.h).
+ */
+void defaultDeviceConfig()
+{
+  Serial.print(F("\n[WARNING]: USING DEFAULT VALUES!"));
+  //storeString(String(DEFAULT_HOSTNAME), Device_config.host_name);
+  if (strlen(String(DEFAULT_HOSTNAME).c_str()) < sizeof(Device_config.host_name) - 1)
+    strcpy(Device_config.host_name, String(DEFAULT_HOSTNAME).c_str());
+  else
+    strncpy(Device_config.host_name, String(DEFAULT_HOSTNAME).c_str(), sizeof(Device_config.host_name) - 1);
+
+  //Device_config.air_value = DEF_CAL_AIR;
+  //Device_config.wat_value = DEF_CAL_WAT;
+  Device_config.ap_mode   = DEFAULT_TO_AP;  
 }
