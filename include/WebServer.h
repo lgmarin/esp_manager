93e4fd4fe573d2d94d66891a8e21351cd79f8512
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-09-27 14:35:29 -0300

    Insert page processors

--- include/WebServer.h
+++ include/WebServer.h
@@ -8,6 +8,10 @@ class WebServer
 {
 private:
     AsyncWebServer _server = AsyncWebServer(80);
+    static String _indexPageProcessor(const String& var);
+    static String _configPageProcessor(const String& var);
+    static String _wifiPageProcessor(const String& var);
+    static void _notFoundHandler(AsyncWebServerRequest *request);
     void _bindServerHandlers();
 public:
     void begin();
--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -2,24 +2,120 @@
 #include "ConfigManager.h"
 #include <LittleFS.h>
 
+/*
+*     HTML PRE-PROCESSORS
+*/
+
+String WebServer::_indexPageProcessor(const String& var){
+    if(var == "HOST_NAME"){
+        return getHostName();
+    }
+    else if(var == "DEVICE_IP"){
+        return getIpAddress();
+    }
+    // else if(var == "DHT_T"){
+    //     return String(airTemp());
+    // }
+    // else if(var == "DHT_H"){
+    //     return String(airHumidity());
+    // }
+    // else if(var == "SOIL_H"){
+    //     return String(soilHumidity());
+    // }
+    else if(var == "CONN_STATUS"){
+        if (getMode() == "Conectado")
+        {
+            return "fc1505";
+        }
+        return "000000";
+    }
+    return String();
+}
+
+String WebServer::_configPageProcessor(const String& var){
+    if(var == "MODE"){
+        return getMode();
+    }
+    else if(var == "SSID"){
+        return getSSID();
+    }
+        else if(var == "HOST_NAME"){
+        return getHostName();
+    }
+    else if(var == "DEVICE_IP"){
+        return getIpAddress();
+    }
+    else if (var == "MAC")
+    {
+        return getMacAddress();
+    }
+    else if (var == "AP_MODE")
+        if(Device_config.ap_mode)
+        {
+        return "checked";
+        } 
+        else
+        {
+        return "";
+        }
+    // else if (var == "AIR_VAL")
+    // {
+    //     return String(Device_config.air_value);
+    // }
+    // else if (var == "WAT_VAL")
+    // {
+    //     return String(Device_config.wat_value);
+    // }  
+    return String();
+}
+
+String WebServer::_wifiPageProcessor(const String& var){
+    if(var == "HOST_NAME"){
+        return getHostName();
+    }
+    else if(var == "DEVICE_IP"){
+        return getIpAddress();
+    }
+    return String();
+}
+
+void WebServer::_notFoundHandler(AsyncWebServerRequest *request){
+    String message = "File Not Found\n\n";
+    message += "URI: ";
+    message += request->url();
+    message += "\nMethod: ";
+    message += (request->method() == HTTP_GET) ? "GET" : "POST";
+    message += "\nArguments: ";
+    message += request->args();
+    message += "\n";
+
+    for (uint8_t i = 0; i < request->args(); i++)
+    {
+        message += " " + request->argName(i) + ": " + request->arg(i) + "\n";
+    }
+    request->send(404, "text/plain", message);
+}
+
 void WebServer::_bindServerHandlers()
 {
     _server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        AsyncWebServerResponse * response = request->beginResponse(LittleFS, "/index.html", "text/html", false);
+        AsyncWebServerResponse * response = request->beginResponse(LittleFS, "/index.html", "text/html", false, _indexPageProcessor);
         request->send(response);
     });
 
-    server.on("/generate_204", HTTP_GET, [](AsyncWebServerRequest * request){
+    _server.on("/generate_204", HTTP_GET, [](AsyncWebServerRequest * request){
         request->redirect("/index.html");
     }).setFilter(ON_AP_FILTER);
 
-    server.on("/fwlink", HTTP_GET, [](AsyncWebServerRequest * request){
+    _server.on("/fwlink", HTTP_GET, [](AsyncWebServerRequest * request){
         request->redirect("/index.html");
     }).setFilter(ON_AP_FILTER);
 
     // server.on("/read-sensors", HTTP_GET, [](AsyncWebServerRequest *request){
     //     request->send(200, "application/json", readSensorsJSON());
     // });
+
+    _server.onNotFound(_notFoundHandler);
 }
 
 
