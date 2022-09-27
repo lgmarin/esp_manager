16f9279d0142bbf99c4923fe8f788b796eccaf0e
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-09-27 14:24:49 -0300

    Add Index page and AP Handlers

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -2,14 +2,24 @@
 #include "ConfigManager.h"
 #include <LittleFS.h>
 
-
 void WebServer::_bindServerHandlers()
 {
-    _server.on("/", HTTP_GET, []AsyncWebServerRequest *request)
-    {
+    _server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
         AsyncWebServerResponse * response = request->beginResponse(LittleFS, "/index.html", "text/html", false);
         request->send(response);
-    }
+    });
+
+    server.on("/generate_204", HTTP_GET, [](AsyncWebServerRequest * request){
+        request->redirect("/index.html");
+    }).setFilter(ON_AP_FILTER);
+
+    server.on("/fwlink", HTTP_GET, [](AsyncWebServerRequest * request){
+        request->redirect("/index.html");
+    }).setFilter(ON_AP_FILTER);
+
+    // server.on("/read-sensors", HTTP_GET, [](AsyncWebServerRequest *request){
+    //     request->send(200, "application/json", readSensorsJSON());
+    // });
 }
 
 
@@ -18,7 +28,7 @@ void WebServer::begin()
     _bindServerHandlers();
 
     Serial.print(F("\n[INFO]: Initializing WebServer..."));
-    server.begin();
+    _server.begin();
 
-    server.serveStatic("/", LittleFS, "/");
+    _server.serveStatic("/", LittleFS, "/");
 }
